87ca33c295fbcae1568f67e281e5aa0e09af96a3
houdaifa <houdaifazaidi04@gmail.com>
2026-06-18 13:16:40 +0100

    redeploy

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::get('/articles/{id}', [RProduitController::class, 'show'])->name('article
 
 Auth::routes();
 
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
